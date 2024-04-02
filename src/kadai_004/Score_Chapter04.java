c3f6ccf3e3c519b9fa1e0b3545bb8ec260317a82
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2024-04-03 01:16:35 +0900

    課題004の追加

---
+++ src/kadai_004/Score_Chapter04.java
@@ -0,0 +1,21 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// それぞれの点数の出力
+		System.out.println("Aさん10点");
+		System.out.println("Bさん20点");
+		System.out.println("Cさん30点");
+		System.out.println("Dさん40点");
+		System.out.println("Eさん50点");
+		System.out.println("Fさん60点");
+		System.out.println("Gさん70点");
+		System.out.println("Hさん80点");
+		System.out.println("Iさん90点");
+		System.out.println("Jさん100点");
+		//平均点の出力
+		System.out.println((10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100) / 10);
+	}
+
+}
