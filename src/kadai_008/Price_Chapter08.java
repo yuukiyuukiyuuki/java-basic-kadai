749756c2300fa6a21df46ff675a362bac414182e
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2024-04-05 23:10:48 +0900

    課題008の追加

---
+++ src/kadai_008/Price_Chapter08.java
@@ -0,0 +1,25 @@
+package kadai_008;
+
+public class Price_Chapter08 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//年代の変数
+		int userAge = 3;
+		//料金の変数
+		int serviceCost = 3000;
+		
+		switch(userAge) {
+			case 1 -> System.out.println("10代の料金は1000円");
+			case 2 -> System.out.println("20代の料金は2000円");
+			case 3 -> System.out.println("30代の料金は" + serviceCost + "円");
+			case 4 -> System.out.println("40代の料金は3000円");
+			case 5 -> System.out.println("50代の料金は4000円");
+			case 6 -> System.out.println("60代の料金は4000円");
+			case 7 -> System.out.println("70代の料金は4000円");
+			case 8 -> System.out.println("80代の料金は5000円");
+			default -> System.out.println("その他の料金は500円");
+		}
+	}
+
+}
