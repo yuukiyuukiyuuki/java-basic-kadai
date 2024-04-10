9a44691804013364523d2bdb49de27e7f05c95f7
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2024-04-11 01:24:33 +0900

    課題24の追加

---
+++ src/kadai_024/JyankenExec_Chapter24.java
@@ -0,0 +1,11 @@
+package kadai_024;
+
+
+public class JyankenExec_Chapter24 {
+	public static void main(String[] args) {
+		//じゃんけんのインスタンス
+		Jyanken_Chapter24 jyanken = new Jyanken_Chapter24();
+		//結果
+		jyanken.playGame();
+	}
+}
---
+++ src/kadai_024/Jyanken_Chapter24.java
@@ -0,0 +1,60 @@
+package kadai_024;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Jyanken_Chapter24 {
+	public String getMyChoice() {
+		System.out.println("自分のじゃんけんの手を入力しましょう");
+		System.out.println("グーはrockのrを入力しましょう");
+		System.out.println("チョキはscissorsのsを入力しましょう");
+		System.out.println("パーはpaperのpを入力しましょう");
+
+		// Scannerクラスの生成
+		Scanner scanner = new Scanner(System.in);
+
+		// 入力内容の取得
+		String input = scanner.nextLine();
+
+		// じゃんけんの手を判定
+		if (input.equals("r") || input.equals("s") || input.equals("p")) {
+			scanner.close();
+		} else {
+			System.out.println("じゃんけんの手ではありません。再入力してください。");
+		}
+		return input;
+	}
+
+	// 対戦相手のじゃんけんの手を乱数で選ぶ
+	public String getRandom() {
+		String[] rspArray = { "r", "s", "p" };
+		String random = rspArray[(int) Math.floor(Math.random() * 3)];
+		return random;
+	}
+
+	// じゃんけんメソッド
+	public void playGame() {
+		HashMap<String, String> type = new HashMap<String, String>();
+
+		type.put("r", "グー");
+		type.put("s", "チョキ");
+		type.put("p", "パー");
+
+		// 自分と対戦相手のじゃんけんの手を取得
+		String myChoice = getMyChoice();
+		String choice = getRandom();
+
+		System.out.println(
+				"自分の手は" + type.get(myChoice) + "," + "対戦相手の手は" + type.get(choice));
+
+		// 自分と対戦相手のじゃんけんの手の比較
+		if (myChoice.equals(choice)) {
+			System.out.println("あいこです");
+		} else if ((myChoice.equals("r") && choice.equals("s")) || (myChoice.equals("s")
+				&& choice.equals("p")) || (myChoice.equals("p") && choice.equals("r"))) {
+			System.out.println("自分の勝ちです");
+		} else {
+			System.out.println("自分の負けです");
+		}
+	}
+}
